06ff824f9a2d483fba5de921bcd516982909f165
apurup <apurup2011@gmail.com>
2018-07-24 07:17:22 +0530

    commit 4

---
+++ navbar.js
@@ -0,0 +1,14 @@
+function showProfile() {
+    window.location.href = "profile.html";
+
+}
+function showHome() {
+    window.location.href = "index.html";
+}
+function showChat() {
+    document.getElementById('chatSpace').scrollIntoView();
+
+}
+function openFiles() {
+    document.getElementById("fileLoader").click();
+}
